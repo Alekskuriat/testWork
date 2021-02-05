7132c3ddc1818558ea4ef9b55659d6bcb4c621d0
Aleks <kuriatae@mail.ru>
2021-02-05 16:46:30 +0700

    init dimensional array

--- src/main/java/packageOne/testClass.java
+++ src/main/java/packageOne/testClass.java
@@ -3,6 +3,7 @@ package packageOne;
 public class testClass {
     public static void main(String[] args){
         initArray(17);
+        initDimensionalArray(12,5);
 
     }
 
@@ -12,6 +13,19 @@ public class testClass {
             arr[i] = (int)(Math.random()*100);
             System.out.println("arr[" + i + "] = " + arr[i]);
         }
+    }
+
+    private static void initDimensionalArray(int varA, int varB) {
+
+        int[][] table = new int[varA][varB];
+        for (int i = 0; i < varA; i++) {
+            for (int j = 0; j < varB; j++) {
+                table[i][j] = (int)(Math.random()*100);
+                System.out.print("arr[" + i + "," + j + "] = " + table[i][j] + "; ");
+            }
+            System.out.println();
+        }
+
     }
 
 }
